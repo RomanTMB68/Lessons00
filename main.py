87d7d58ec47f531eeffcb3c0efb8bddb36e0e1ad
RomanTMB68 <medoedtmb@gmail.com>
2024-05-31 14:30:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+a="Helloworld"
+
+print(len(a))
+
+first=10
+second=7
+
+summa=(first+second)
+diff=(first-second)
+
+print(summa, diff)
+
+third=2
+
+mean=((first+second+third)//3)
+
+print(mean)
+
+first_string="вторник"
+second_string="понедельник"
+
+print(second_string,', ',first_string)
+del(a)
+a=5
+b=4
+c=6
+f=(a*b+a*c)**3/2
+print(f)
